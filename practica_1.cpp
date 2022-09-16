82db706ec6038b8a3d0aff52be747aec927ffd4b
R <rsanchez628@alumno.uned.es>
2022-09-16 10:01:43 +0200

    Practica 1. Salida de datos por consola

---
+++ practica_1.cpp
@@ -0,0 +1,14 @@
+/*************************************
+* NOMBRE: #Ricardo#
+* PRIMER APELLIDO: #Sanchez#
+* EMAIL: #rsanchez628@alumno.uned.es#
+*************************************/
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Ricardo\n");
+  printf("Sanchez\n");
+  printf("rsanchez628@alumno.uned.es\n");
+}
